645488ea5cb90f1ebd3714ef2e00f2f04a03bd6b
MD Shoaib Khan <abrohoman019@gmail.com>
2025-06-05 18:46:59 +0600

    refactor: update dependencies and configuration for Tailwind CSS
    - Removed duplicate next-auth dependency in package.json
    - Updated postcss.config.js to use tailwindcss and autoprefixer directly
    - Changed tailwind.config.js to use content instead of purge and removed JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
